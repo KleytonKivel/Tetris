31a7e2e6ce9c6b92788c09f51ac154d6ccca2a37
Kleyton Kivel Cuenca <kleytonkivel@gmail.com>
2019-12-03 20:35:01 -0300

    Criação da pasta no Git e inicio

---
+++ src/Control/Main.java
@@ -0,0 +1,21 @@
+package Control;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int tabX = 10;
+		int tabY = 22;
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		for (int i = 0; i < 15; i++) {
+			System.out.println("			|   	       |");
+		}
+		System.out.println("			|______________|");
+
+	}
+
+}
---
+++ src/Model/Tabuleiro.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Tabuleiro {
+	
+	public void print() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		for (int i = 0; i < 15; i++) {
+			System.out.println("			|   	       |");
+		}
+		System.out.println("			|______________|");
+
+	}
+
+	
+	
+
+}
